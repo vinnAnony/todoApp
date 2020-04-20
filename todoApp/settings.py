e60e91e5ed91fc08a2e19d6006dddf2981667dad
Vinn-jexi <vinniehaz16>
2020-04-21 02:47:09 +0300

    Added deployment files

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'z--cid%!!2y_o4j2hp**zv92b(+#9@03#^(y5ns-qe7)98&=p4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -122,3 +123,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
             os.path.join(BASE_DIR,'static'),
             ]
+
+django_heroku.settings(locals())
